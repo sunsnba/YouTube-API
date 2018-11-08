ddecbefcf587bd337291b97b5f4ec1d2144bac32
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2018-11-07 16:04:08 -0800

    header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Components/Header';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div className="App">
+        <Header/>
         <header className="App-header">
           <p>
             Edit <code>src/App.js</code> and save to reload.

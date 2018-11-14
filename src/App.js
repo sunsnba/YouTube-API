73ffec33fb684fe0d40874372f4532e6dcff97c2
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2018-11-14 13:46:44 -0800

    now serving on port 3006 and added the video list components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from './Components/Header';
 import VideoForm from './Components/VideoForm';
 import Player from './Components/Player';
 import './App.css';
@@ -9,28 +8,14 @@ class App extends Component {
     super(props)
   }
 
-//  componentDidMount () {
-//    fetch(//link here, {
-//      method: 'GET'
-//    )}
-//    .then(response => response.json())
-//    .then(json => {
 
-//    })
-//    .catch(error => {
-//      console.log(error)
-//    }); 
-//  }
 // key=API_KEY AIzaSyAtaP3I7LAK-M5imwVcQUOh3c5-9sofvFk
 
   render() {
     return (
       <div className="App">
-        <Header/>
         <VideoForm />
-        <header className="App-header">
         <Player />
-        </header>
 
       </div>
     );

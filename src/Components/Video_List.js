520939e6bf86c5293ade95babd0f93f04e6dfb34
Scott Bengtson <scottbengtson@Scotts-MacBook-Air.local>
2018-11-14 15:35:33 -0800

    app now working properly

--- src/Components/Video_List.js
+++ src/Components/Video_List.js
@@ -2,9 +2,9 @@ import React from 'react';
 import VideoListItem from './Video_List_Item';
 
 const VideoList = (props)=> {
-
+        console.log(props.videos, 'props.videos')
     const videoItem = props.videos.map((video) => {
-        return <VideoListItem onVideoSelect={props.onVideoSelectedVideo} key={video.etag} video={video} />
+        return <VideoListItem onVideoSelect={props.onSelectedVideo} key={video.etag} video={video} />
     });
 
     return (
